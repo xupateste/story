0db92a2ef1c741f824c3c45f13f5c72c45da6915
ha <some@email.com>
2024-03-27 16:41:52 -0500

    try rddd1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  
+  async redirects() {
+    return [
+      {
+        source: '/qr',
+        destination: 'https://linkpop.com/ferrisur',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 };
